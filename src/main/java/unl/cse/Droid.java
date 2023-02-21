fa4b7cee5be0bf4f0841430ac2d2044daafdb088
Chris Bourke <cbourke@cse.unl.edu>
2023-02-21 13:36:02 -0600

    docs, formatting updates

--- src/main/java/unl/cse/Droid.java
+++ src/main/java/unl/cse/Droid.java
@@ -8,13 +8,13 @@ public class Droid {
 
 	private final String designation;
 	private final String type;
-	
+
 	public Droid(String designation, String type) {
 		this.designation = designation;
 		this.type = type;
 	}
-	
+
 	public String toString() {
-		return this.designation + " (" + this.type + ")";
+		return String.format("%s (%s)", this.designation, this.type);
 	}
 }
--- src/main/java/unl/cse/Undergraduate.java
+++ src/main/java/unl/cse/Undergraduate.java
@@ -1,5 +1,9 @@
 package unl.cse;
 
+/**
+ * Models an undergraduate student
+ *
+ */
 public class Undergraduate extends Student {
 
 	public Undergraduate(String nuid, String lastName, String firstName) {
